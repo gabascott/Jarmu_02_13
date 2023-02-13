1c479f2724656af669e081d675c9f31db10efa94
Gábor Somoskői <smskgbr@gmail.com>
2023-02-13 14:54:51 +0100

    2. commit

--- src/modell/Auto.java
+++ src/modell/Auto.java
@@ -1,14 +1,34 @@
 package modell;
 
+import java.util.Random;
+
 public class Auto extends Jarmu{
     private boolean defekt;
 
-    public Auto() {}
-    
-    public void kereketCserel(){};
-    
-    @Override
-    public boolean halad(){
-        return true;
+    public Auto(){
+        defekt = false;
     };
+
+    public void setDefekt(boolean defekt) {
+        this.defekt = defekt;
+    }
+    
+    public void kereketCserel(){
+        if (defekt == true) {
+            setDefekt(false);
+        }
+    };
+
+    @Override
+    public boolean halad() {
+        Random rnd = new Random();
+        int esely = rnd.nextInt(4);
+        if (esely == 1) {
+            setDefekt(true);
+            return false;
+        }else{
+            return true;
+        }
+    }
+    
 }
--- src/modell/Jarmu.java
+++ src/modell/Jarmu.java
@@ -4,16 +4,48 @@ package modell;
 public abstract class Jarmu {
 
     private boolean beinditva, uzemanyag, megerkezett;
+
+    public Jarmu() {
+        setBeinditva(false);
+        setUzemanyag(true);
+        setMegerkezett(false);
+    }
+
+    public void setBeinditva(boolean beinditva) {
+        this.beinditva = beinditva;
+    }
+
+    public void setUzemanyag(boolean uzemanyag) {
+        this.uzemanyag = uzemanyag;
+    }
+
+    public void setMegerkezett(boolean megerkezett) {
+        this.megerkezett = megerkezett;
+    }
     
-    public void beindit(){};
+    public void beindit(){
+        setBeinditva(true);
+    };
     
-    public void leallit(){};
+    public void leallit(){
+        setBeinditva(false);
+    };
     
     public boolean tankol(){
-        return true;
+        if (beinditva == false) {
+            return true;
+        }else{
+            return false;
+        }
     };
     
     public boolean halad(){
-        return true;
+        if (uzemanyag == true) {
+            setUzemanyag(false);
+            setMegerkezett(true);
+            return true;
+        }else{
+            return false;
+        }
     };;
 }
