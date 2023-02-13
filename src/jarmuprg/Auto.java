202b575817eee97db12fa43fd9339b1617ae600c
Gábor Somoskői <smskgbr@gmail.com>
2023-02-13 13:42:32 +0100

    1. commit

---
+++ src/jarmuprg/Auto.java
@@ -0,0 +1,14 @@
+package jarmuprg;
+
+public class Auto extends Jarmu{
+    private boolean defekt;
+
+    public Auto() {}
+    
+    public void kereketCserel(){};
+    
+    @Override
+    public boolean halad(){
+        return true;
+    };
+}
---
+++ src/jarmuprg/Jarmu.java
@@ -0,0 +1,15 @@
+
+package jarmuprg;
+
+public abstract class Jarmu {
+
+    private boolean beinditva, uzemanyag, megerkezett;
+    
+    public void beindit(){};
+    
+    public void leallit(){};
+    
+    public void tankol(){};
+    
+    public abstract boolean halad();;
+}
